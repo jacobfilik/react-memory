9f1512c4f93797163ba0ca820281e2ac7504a111
Jacob Filik <jacob.filik@gmail.com>
2022-10-04 21:01:23 +0100

    show pairs of cards

--- src/App.js
+++ src/App.js
@@ -1,43 +1,10 @@
 import "./App.css";
-import getImageMap from "./Cards";
-
-import { useState } from "react";
-
-var randomProperty = function (obj) {
-  var keys = Object.keys(obj);
-  return obj[keys[(keys.length * Math.random()) << 0]];
-};
-
-const Card = (props) => {
-  const [flipped, setFlipped] = useState(false);
-  const handleClick = () => {
-    setFlipped(!flipped);
-    console.log("click");
-  };
-
-  return (
-    <div
-      className={flipped ? "flipped-card" : "flip-card"}
-      onClick={handleClick}
-    >
-      <div className="flip-card-inner">
-        <div className="flip-card-front"></div>
-        <div className="flip-card-back">
-          <img src={randomProperty(getImageMap())} alt="" />
-        </div>
-      </div>
-    </div>
-  );
-};
+import Cards from "./Cards";
 
 function App() {
   return (
     <div className="game">
-      <div>
-        {[...Array(54).keys()].map((number) => (
-          <Card key={number} />
-        ))}
-      </div>
+      <Cards />
     </div>
   );
 }
--- src/Cards.js
+++ src/Cards.js
@@ -1,3 +1,5 @@
+import "./Cards.css";
+
 import star from "./images/star.svg";
 import heart from "./images/heart.svg";
 import bee from "./images/bee.svg";
@@ -8,6 +10,25 @@ import happy from "./images/happy.svg";
 import leaf from "./images/leaf.svg";
 import letter from "./images/letter.svg";
 import paw from "./images/paw.svg";
+
+import React from "react";
+import { useState } from "react";
+
+function shuffleArray(array) {
+  for (let i = array.length - 1; i > 0; i--) {
+    const j = Math.floor(Math.random() * (i + 1));
+    [array[i], array[j]] = [array[j], array[i]];
+  }
+}
+
+function getCardStates() {
+  const imageMap = getImageMap();
+  const imageArray = Array.from(Object.entries(imageMap));
+  const imageArrayCopy = [...imageArray];
+  const imarray = imageArray.concat(imageArrayCopy);
+  shuffleArray(imarray);
+  return imarray;
+}
 
 function getImageMap() {
   return {
@@ -24,4 +45,40 @@ function getImageMap() {
   };
 }
 
-export default getImageMap;
+const Card = (props) => {
+  const [flipped, setFlipped] = useState(false);
+  const handleClick = () => {
+    setFlipped(!flipped);
+    console.log("click " + props.number);
+  };
+
+  return (
+    <div
+      className={flipped ? "flipped-card" : "flip-card"}
+      onClick={handleClick}
+    >
+      <div className="flip-card-inner">
+        <div className="flip-card-front"></div>
+        <div className="flip-card-back">
+          <img src={props.image[1]} alt="" />
+        </div>
+      </div>
+    </div>
+  );
+};
+
+const Cards = (props) => {
+  const imageArray = getCardStates();
+
+  //const onClick = () =>
+  //start to pass down state on props
+  return (
+    <div>
+      {imageArray.map((image, index) => (
+        <Card key={index} number={index} image={image} />
+      ))}
+    </div>
+  );
+};
+
+export default Cards;
